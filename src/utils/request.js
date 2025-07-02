0ebf45a838773969d608f9f23dd7071fc5618082
养乐多lsj <11275955+yakult-lsj@user.noreply.gitee.com>
2025-07-02 16:00:13 +0800

    图片联系

--- src/utils/request.js
+++ src/utils/request.js
@@ -20,7 +20,7 @@ axios.defaults.headers["Content-Type"] = "application/json;charset=utf-8";
 // 创建axios实例
 const service = axios.create({
   // axios中请求配置有baseURL选项，表示请求URL公共部分
-  baseURL: import.meta.env.VITE_BASE_PATH + import.meta.env.VITE_APP_BASE_API,
+  baseURL: import.meta.env.VITE_APP_BASE_API,
   // 超时
   timeout: 10000,
 });
